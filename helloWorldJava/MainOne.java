5765048d57e5edb52991c6f41c60cfcbbcb57e1c
jose <jencar22@wgu.edu>
2026-05-10 12:14:58 -0600

    Organized Java practice files and added new exercises

--- helloWorldJava/MainOne.java
+++
@@ -1,5 +0,0 @@
-public class MainOne{
-    public static void main (String[] args) {
-   
-    }
-}
--- helloworld/SyntaxPractice.java
+++ helloworld/SyntaxPractice.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main{
+public class SyntaxPractice{
     public static void main (String[] args) {
             
         Scanner scanner = new Scanner(System.in);
